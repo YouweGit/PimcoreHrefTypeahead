4679dc778ba2d627eb6a8289a440317421a02515
Joris Ros <joris.ros@gmail.com>
2018-02-05 13:18:56 +0100

    Commit the bundle structure for Pimcore5

---
+++ README.md
@@ -0,0 +1,28 @@
+# Href typeahead field type
+Adds typeahead functionality to href fields using a modified default search functionality
+
+I temporary made documents and assets read only field in field configurator to prevent users to
+select assets as a valid link at static/js/pimcore/object/classes/data/hrefTypeahead.js, but i didn't want 
+to remove it because maybe we will add functionality to support assets and document
+Also i added validation to prevent users to select more then one class
+### Install
+```bash
+composer require youwe/pimcore-href-typeahead
+```
+### Files 
+<dl>
+  <dt><strong>static/js/pimcore/object/classes/data/hrefTypeahead.js</strong></dt>
+  <dd>An exact copy of default href</dd>
+  
+  <dt><strong>static/js/pimcore/object/tags/hrefTypeahead.js</strong></dt>
+  <dd>An exact copy of default href, with the change that textbox is now a combo with autocomplete provided by SearchController::findAction</dd>
+    
+  <dt><strong>models/Pimcore/Model/Object/ClassDefinition/Data/HrefTypeahead.php</strong></dt>
+  <dd>An exact copy of default href</dd>
+  
+  <dt><strong>HrefTypeahead/controllers/SearchController.php</strong></dt>
+  <dd>A controller containing autocomplete source</dd>
+</dl>
+
+### Limitations
+* It only supports one object linked in href
---
+++ lib/HrefTypeahead/SearchBuilder.php
@@ -0,0 +1,279 @@
+<?php
+
+
+namespace HrefTypeahead;
+
+
+use Pimcore\Model\User;
+
+class SearchBuilder
+{
+    /** @var  array */
+    private $types;
+    /** @var  array */
+    private $subTypes;
+    /** @var  User */
+    private $user;
+    /** @var  array */
+    private $classNames;
+    /** @var  string */
+    private $query;
+    /** @var  int */
+    private $start;
+    /** @var  int */
+    private $limit;
+    /** @var  array */
+    private $fields;
+    /** @var array */
+    private $filter;
+    /** @var  array */
+    private $tagIds;
+    /** @var  bool */
+    private $considerChildTags;
+    /** @var array */
+    private $sortSettings;
+
+    /**
+     * @return SearchBuilder
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * @return SearchService
+     */
+    public function build()
+    {
+        return new SearchService($this);
+    }
+
+    /**
+     * @return array
+     */
+    public function getTypes()
+    {
+        return $this->types;
+    }
+
+    /**
+     * @param array $types
+     * @return SearchBuilder
+     */
+    public function withTypes(array $types)
+    {
+        $this->types = $types;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSubTypes()
+    {
+        return $this->subTypes;
+    }
+
+    /**
+     * @param array $subTypes
+     * @return SearchBuilder
+     */
+    public function withSubTypes(array $subTypes)
+    {
+        $this->subTypes = $subTypes;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     * @return SearchBuilder
+     */
+    public function withUser(User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getClassNames()
+    {
+        return $this->classNames;
+    }
+
+    /**
+     * @param array $classNames
+     * @return SearchBuilder
+     */
+    public function withClassNames($classNames)
+    {
+        $this->classNames = $classNames;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @param string $query
+     * @return SearchBuilder
+     */
+    public function withQuery($query)
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    /**
+     * @param int $start
+     * @return SearchBuilder
+     */
+    public function withStart($start)
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param int $limit
+     * @return SearchBuilder
+     */
+    public function withLimit($limit)
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    /**
+     * @param array|null $fields
+     * @return SearchBuilder
+     */
+    public function withFields($fields)
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    /**
+     * @param array|null $filter
+     * @return SearchBuilder
+     */
+    public function withFilter($filter)
+    {
+        $this->filter = $filter;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTagIds()
+    {
+        return $this->tagIds;
+    }
+
+    /**
+     * @param array|null $tagIds
+     * @return SearchBuilder
+     */
+    public function withTagIds($tagIds)
+    {
+        $this->tagIds = $tagIds;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isConsiderChildTags()
+    {
+        return $this->considerChildTags;
+    }
+
+    /**
+     * @param boolean $considerChildTags
+     * @return SearchBuilder
+     */
+    public function withConsiderChildTags($considerChildTags)
+    {
+        $this->considerChildTags = $considerChildTags;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSortSettings()
+    {
+        return $this->sortSettings;
+    }
+
+    /**
+     * @param array $sortSettings
+     * @return $this
+     */
+    public function withSortSettings($sortSettings)
+    {
+        $this->sortSettings = $sortSettings;
+        return $this;
+    }
+
+}
