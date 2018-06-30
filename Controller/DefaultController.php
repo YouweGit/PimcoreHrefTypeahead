db7e4bffb17e067a15baabe765b32c8ebf187258
Joris Ros <joris.ros@gmail.com>
2018-06-30 21:39:54 +0200

    Copy raw pimcore4 search action into new controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -27,7 +27,92 @@ class DefaultController extends FrontendController
      */
     public function findAction(Request $request)
     {
-        return new Response('Hello world from pimcore_href_typeahead');
+        $sourceId = $this->getParam('sourceId');
+        $sourceClassName = $this->getParam('className');
+        $valueIds = $this->getParam('valueIds');
+        $fieldName = $this->getParam('fieldName');
+        $source = null;
+        // We know what object it is and we can get its type by id
+        if ($sourceId) {
+            $source = Object\Concrete::getById($sourceId);
+        } elseif ($sourceClassName) { // We dont know what type it is by we know its class, strange but nice-path requires a specific source object
+            $classFullName = "\\Pimcore\\Model\\Object\\$sourceClassName";
+            $source = new $classFullName();
+        }
+        // Don`t do anything without valid source object
+        if (!$source || !$fieldName) {
+            $this->_helper->json(['data' => [], 'success' => false, 'total' => 0]);
+        }
+
+        /** @var HrefTypeahead $fd */
+        $fd = $source->getClass()->getFieldDefinition($fieldName);
+        if (!$fd || !$fd->getFieldtype() === 'hrefTypeahead' || !$fd->getObjectsAllowed() || count($fd->getClasses()) !== 1) {
+            throw new \InvalidArgumentException('This function can only be called from hrefTypeahead field with one class attached');
+        }
+        $className = current($fd->getClasses())['classes'];
+        // This is a special case when the field is loaded for the first time or they are loaded from
+        if ($valueIds) {
+            $valueObjs = [];
+            foreach (explode_and_trim(',', $valueIds) as $valueId) {
+                $valueObjs[] = Object\Concrete::getById($valueId);
+            }
+            if (!$valueObjs) {
+                $this->_helper->json(['data' => [], 'success' => false, 'total' => 0]);
+            }
+            $elements = [];
+            foreach ($valueObjs as $valueObj) {
+                $label = $this->getNicePath($fd, $valueObj, $source);
+                $elements[] = $this->formatElement($valueObj, $label);
+            }
+            $this->_helper->json(['data' => $elements, 'success' => true, 'total' => count($elements)]);
+        }
+        // This means that we have passed the values ids
+        // but the field is empty this is common when the field is empty
+        // We don't need to continue looping
+        elseif (!$valueIds && $this->hasParam('valueIds')) {
+            $this->_helper->json(['data' => [], 'success' => true, 'total' => 0]);
+        }
+        $filter = $this->getParam('filter') ? \Zend_Json::decode($this->getParam('filter')) : null;
+        $considerChildTags = $this->getParam('considerChildTags') === 'true';
+        $sortingSettings = \Pimcore\Admin\Helper\QueryParams::extractSortingSettings($this->getAllParams());
+        $searchService = SearchBuilder::create()
+            ->withUser($this->getUser())
+            ->withTypes(['object'])
+            ->withSubTypes(['object'])
+            ->withClassNames([$className])
+            ->withQuery($this->getParam('query'))
+            ->withStart((int)$this->getParam('start'))
+            ->withLimit((int)$this->getParam('limit'))
+            ->withFields($this->getParam('fields'))
+            ->withFilter($filter)
+            ->withTagIds($this->getParam('tagIds'))
+            ->withConsiderChildTags($considerChildTags)
+            ->withSortSettings($sortingSettings)
+            ->build();
+        $searcherList = $searchService->getListingObject();
+        /** @var \Pimcore\Model\Search\Backend\Data[] $hits */
+        $hits = $searcherList->load();
+        $elements = [];
+        foreach ($hits as $hit) {
+            /** @var AbstractElement $element */
+            $element = Element\Service::getElementById($hit->getId()->getType(), $hit->getId()->getId());
+            if ($element->isAllowed('list')) {
+                if ($element->getType() === 'object') {
+                    $label = $this->getNicePath($fd, $element, $source);
+                } else {
+                    $label = (string)$element;
+                }
+                $elements[] = $this->formatElement($element, $label);
+            }
+        }
+        // only get the real total-count when the limit parameter is given otherwise use the default limit
+        if ($this->getParam('limit')) {
+            $totalMatches = $searcherList->getTotalCount();
+        } else {
+            $totalMatches = count($elements);
+        }
+        $this->_helper->json(['data' => $elements, 'success' => true, 'total' => $totalMatches]);
+        $this->removeViewRenderer();
     }
 
     /**
